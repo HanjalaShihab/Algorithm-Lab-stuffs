f724267c00973d914329fd21be807cfd5d78d678
Hanjala <hanjalashihab1@gmail.com>
2024-11-25 22:36:46 +0600

    fracktional knapsack using quick sort

--- Fracktional_knapsack_using_Quick_sort.c
+++ Fracktional_knapsack_using_Quick_sort.c
@@ -1,84 +1,78 @@
 #include <stdio.h>
 
-struct Item {
+struct item {
+    int it;
     int weight;
-    int value;
+    int profit;
     double ratio;
 };
 
 
-int partition(struct Item items[], int low, int high) {
-    double pivot = items[high].ratio;
+int partition(struct item arr[], int low, int high) {
+    double pivot = arr[high].ratio; 
     int i = low - 1;
 
     for (int j = low; j < high; j++) {
-        if (items[j].ratio >= pivot) {
+        if (arr[j].ratio > pivot) { 
             i++;
-
-            struct Item temp = items[i];
-            items[i] = items[j];
-            items[j] = temp;
+            struct item temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
-
-
-    struct Item temp = items[i + 1];
-    items[i + 1] = items[high];
-    items[high] = temp;
+    struct item temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
 
     return i + 1;
 }
 
-
-void quickSort(struct Item items[], int low, int high) {
+// Quick Sort function
+void quickSort(struct item arr[], int low, int high) {
     if (low < high) {
-        int pivotIndex = partition(items, low, high);
-        quickSort(items, low, pivotIndex - 1);
-        quickSort(items, pivotIndex + 1, high);
+        int pi = partition(arr, low, high); 
+        quickSort(arr, low, pi - 1);      
+        quickSort(arr, pi + 1, high);       
     }
-}
-
-
-double fractionalKnapsack(struct Item items[], int n, int capacity) {
-    quickSort(items, 0, n - 1);
-
-    double totalValue = 0.0;
-    for (int i = 0; i < n; i++) {
-        if (capacity >= items[i].weight) {
-            capacity -= items[i].weight;
-            totalValue += items[i].value;
-        } else {
-            totalValue += items[i].ratio * capacity;
-            break;
-        }
-    }
-    return totalValue;
 }
 
 int main() {
-
-    struct Item items[] = {
-        {10, 60, 0},
-        {20, 100, 0},
-        {30, 120, 0}
+    struct item item1[5] = {
+        {1, 10, 120},
+        {2, 12, 150},
+        {3, 9, 200},
+        {4, 12, 180},
+        {5, 15, 190}
     };
 
-    int n = sizeof(items) / sizeof(items[0]);
-    int capacity = 50;
-
-    for (int i = 0; i < n; i++) {
-        items[i].ratio = (double)items[i].value / items[i].weight;
+    for (int i = 0; i < 5; i++) {
+        item1[i].ratio = (double)item1[i].profit / item1[i].weight;
     }
 
-    printf("Knapsack Capacity: %d\n", capacity);
+  
+    quickSort(item1, 0, 4);
 
-    for (int i = 0; i < n; i++) {
-        printf("Item %d - Weight: %d, Value: %d, Ratio: %.2f\n",
-               i + 1, items[i].weight, items[i].value, items[i].ratio);
+    double maxProfit = 0.0;
+    int currentWeight = 0;
+    int maxWeight = 40;
+
+    // Calculate maximum profit
+    for (int i = 0; i < 5; i++) {
+        if (currentWeight + item1[i].weight <= maxWeight) {
+            currentWeight += item1[i].weight;
+            maxProfit += item1[i].profit;
+        } else {
+            int remainingWeight = maxWeight - currentWeight;
+            maxProfit += remainingWeight * item1[i].ratio;
+            break;
+        }
     }
 
-    double maxValue = fractionalKnapsack(items, n, capacity);
-    printf("\nMaximum value in the knapsack: %.2f\n", maxValue);
+    for (int i = 0; i < 5; i++) {
+        printf("%d %d %d %.2lf\n", item1[i].it, item1[i].weight, item1[i].profit, item1[i].ratio);
+    }
+
+    printf("Maximum Profit: %.2lf\n", maxProfit);
 
     return 0;
 }
